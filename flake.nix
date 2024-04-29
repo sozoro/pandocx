dfd9dd19319a697e4c96ef252566d8418cad30f0
sozoro <46480375+sozoro@users.noreply.github.com>
2024-04-29 12:41:32 +0900

    flaking

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "pandocx";
+
+  inputs = {
+    nixpkgs.url     = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell.url    = "github:numtide/devshell";
+  };
+
+  outputs = { self, ... }@inputs: (inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    imports = [
+      inputs.devshell.flakeModule
+    ];
+    systems   = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+    perSystem = { pkgs, system, ... }: rec {
+      packages.default  = pkgs.callPackage ./default.nix {};
+      devshells.default = {
+        packages = [ packages.default ];
+      };
+    };
+  }) // {
+    name         = "pandocx";
+    nixosModules = rec {
+      addpkg = { pkgs, ... }: {
+        nixpkgs.config = {
+          packageOverrides = oldpkgs: let newpkgs = oldpkgs.pkgs; in {
+            "${self.name}" = self.packages."${pkgs.system}".default;
+          };
+        };
+      };
+
+      install = { pkgs, ... }: (addpkg { inherit pkgs; }) // {
+        environment.systemPackages = [ pkgs."${self.name}" ];
+      };
+    };
+  };
+}
